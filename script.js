6a2a382bcfe3eced21c90075546d037f6c4e049a
Shajidaa <shajidaakterlopa101@gmail.com>
2025-09-13 08:32:09 +0600

    js file add

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const addTransaction=()=>{
+  
+  const current_balance_amount=document.getElementById('current_balance_amount').innerText
+ const income_amount  =document.getElementById('Income_amount').innerText;
+ const expense_amount=document.getElementById('expense_amount').innerText;  
+ const amount=document.getElementById('amount')
+ 
+}
